36113ddb4711413b2fd2bdb0c7bb39e89d60ef89
Jason Miller <jasonmiller@synacor.com>
2016-10-22 23:42:38 -0400

    Switch to ava for tests, and add tests for `series()`, `parallel()` and `map()`.

--- test/index.js
+++ test/index.js
@@ -1,16 +1,50 @@
-import assert from 'assert';
-import { series, parallel } from '../src';
+import test from 'ava';
+import { spy } from 'sinon';
+import { series, parallel, map } from '..';
 
-assert.equal(typeof series, 'function');
+const get = v => Promise.resolve(v);
 
-assert.equal(typeof parallel, 'function');
+const sleep = time => new Promise( r => setTimeout(r, time) );
 
-parallel([
-	() => Promise.resolve('a'),
-	() => Promise.resolve('b')
-]).then( out => {
-	assert.deepEqual(out, ['a', 'b']);
 
-	console.log('✅ success');
-	process.exit(0);
+test('series', async t => {
+	t.is(typeof series, 'function');
+
+	t.deepEqual(
+		await series([
+			async () => await get(1),
+			async () => await get(2)
+		]),
+		[1, 2]
+	);
+});
+
+
+test('parallel', async t => {
+	t.is(typeof parallel, 'function', 'should be a function');
+
+	t.deepEqual(
+		await parallel([
+			async () => await get(1),
+			async () => await get(2)
+		]),
+		[1, 2]
+	);
+});
+
+
+test('map', async t => {
+	t.is(typeof map, 'function');
+
+	let fn = spy( async value => (await sleep(50), await get(value * 2)) );
+
+	let start = Date.now();
+
+	let out = await map([1, 2, 3], fn);
+
+	t.deepEqual(out, [2, 4, 6]);
+
+	let elapsed = Date.now() - start;
+
+	t.true(elapsed < 100, 'Should invoke in parallel');
 });
